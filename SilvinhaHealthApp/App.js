0f8c28dc0c765e8b270f16a6df429a037415b98a
DanielVianaC <daniel.carvalho85@etec.sp.gov.br>
2025-02-27 12:27:12 -0300

    Estilação da tela inicial

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.titlebox}>
-        <Text style={styles.titleText}>Silvinha</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleBox}>
+        <Text style={styles.titleText}>Silvinha Health App</Text>
       </View>
 
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e0e5e5',
-    paddingTop: 50
+    backgroundColor: '#edf2f4',
   },
+  titleBox: {
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  height: 100,
+  backgroundColor: '#ef233c',
+  borderBottomStartRadius: 25,
+  borderBottomEndRadius: 25,
+  },
+
+  titleText: {
+    color: '#edf2f4',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
 });
